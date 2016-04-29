c7c9ea8129cee5b2fa6fe6881117929121a161be
Ryan Blue <blue@apache.org>
2016-04-29 09:56:07 -0700

    Add filter example.

--- src/main/java/netflix/example/ReadParquetPojo.java
+++ src/main/java/netflix/example/ReadParquetPojo.java
@@ -6,10 +6,15 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.filter2.compat.FilterCompat;
+import org.apache.parquet.filter2.predicate.FilterPredicate;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.io.api.Binary;
 import java.util.Collection;
 
+import static org.apache.parquet.filter2.predicate.FilterApi.binaryColumn;
+import static org.apache.parquet.filter2.predicate.FilterApi.eq;
 import static org.apache.parquet.hadoop.ParquetFileWriter.Mode.OVERWRITE;
 import static org.apache.parquet.hadoop.metadata.CompressionCodecName.GZIP;
 
@@ -47,8 +52,26 @@ public class ReadParquetPojo {
 
       Pojo pojo;
       while ((pojo = reader.read()) != null) {
-        System.err.println(pojo);
+        System.err.println("All records: " + pojo);
       }
     }
+
+    // Read Pojos, but filter down to just Penguins
+    FilterPredicate predicate = eq(binaryColumn("data"), Binary.fromString("penguin"));
+
+    try (ParquetReader<Pojo> reader = AvroParquetReader.<Pojo>builder(dataFile)
+        .withDataModel(new ReflectData(Pojo.class.getClassLoader()))
+        .disableCompatibility() // always use this (since this is a new project)
+        .withFilter(FilterCompat.get(predicate))
+        .withConf(conf)
+        .build()) {
+
+      Pojo pojo;
+      while ((pojo = reader.read()) != null) {
+        System.err.println("Just Penguins: " + pojo);
+      }
+    }
+
+
   }
 }
